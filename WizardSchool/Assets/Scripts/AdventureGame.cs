5ee139c8ed158e542ed2087f8a30783acba68f34
Christopher Brandt <xtopher.brandt@gmail.com>
2019-11-04 21:37:37 -0800

    First commit

---
+++ WizardSchool/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.currentState = startingState;
+        textComponent.text = currentState.GetStoryState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = this.currentState.GetNextStates();
+
+        for ( int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                this.currentState = nextStates[index];
+            }
+        }
+
+        textComponent.text = this.currentState.GetStoryState();
+    }
+
+}
